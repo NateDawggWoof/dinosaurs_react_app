7039ed152864a955d4fbebb25f160d76b60ee28b
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-09-11 15:09:09 +0100

    Minor temporary css changes

--- client/src/components/DinoDetails.js
+++ client/src/components/DinoDetails.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import { useState } from 'react';
 
-const DinoDetails = ({ selectedDinosaur }) => {
+const DinoDetails = ({ selectedDinosaur }, ) => {
+    //doesn't do anything
+    const [closeIcon, setCloseIcon] = useState(null);
 
     // temporary style
     const temporaryDiv = {
         position: 'fixed',
         top: '0',
         left: '0',
+        width: '100%',
+        height: '100%',
+
+        display: 'flex',
+        justify_content: 'center',
+        align_items: 'center',
+        background: "grey",
+        opacity: '0.9'
+
     }
 
+    const closeButton = {
+        position: 'absolute',
+        top: '50px',
+        right: '50px'
+    }
+    //no on-click in the close button
     return (
         <div style={temporaryDiv}>
+             <button style={closeButton}>❌</button> 
             <img alt={selectedDinosaur.name} src={`../image/${selectedDinosaur.img}`} />
             <h2>{selectedDinosaur.name}</h2>
         </div>

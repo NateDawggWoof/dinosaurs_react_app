67675ac68fa718b4bc14947516ef3e4cb2d803d1
mitkousa <uk.dimitar@gmail.com>
2021-09-13 01:18:57 +0100

    small changes on quiz form and score

--- client/src/components/QuizForm.js
+++ client/src/components/QuizForm.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import Carnivore from '../styles/images/Carnivore.png'
 import Herbivore from '../styles/images/Herbivore.png'
 import Omnivore from '../styles/images/Omnivore.png'
+import Score from "../components/Score";
 
 
         const QuizForm = ({allDinosaurs, answers, setValue, onSubmit}) => {
@@ -14,8 +15,6 @@ import Omnivore from '../styles/images/Omnivore.png'
                     <h4>{dino.nickname}</h4>
                     <div style={{position: 'relative',top: 0,bottom: 0,left: 0,right: 0}}>
                     <img style ={{width: 120,height: 120}} src={`../image/${dino.img}`} alt={dino.name}/>
-                    
-                
                     </div>
                         <input onChange={(event)=> setValue(dino,event)} type="radio" id={`${dino.name}-Carnivore`} name={`${dino.name}-Diet`} value="Carnivore" required/>
                         <label htmlFor={`${dino.name}-Diet`}><img src={Carnivore} width = "100" height="50"/></label>

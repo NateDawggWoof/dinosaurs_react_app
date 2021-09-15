a62013c2487cb4479b587029526e073a14dc511e
Jeremy Yoo <jyyoo824@gmail.com>
2021-09-15 15:23:38 +0100

    NamePopUp component create minor

--- client/src/components/NamePopUp.js
+++ client/src/components/NamePopUp.js
@@ -3,7 +3,11 @@ import React from 'react';
 const NamePopUp = () => {
 
     return (
-        <></>
+        <>
+            <div>
+                
+            </div>            
+        </>
     )
 }
 
